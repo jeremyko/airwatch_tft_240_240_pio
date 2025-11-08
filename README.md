081c1eeb1c787f81be80d1845b7417672a0fb246
jung hyun, ko <jeremyko69@gmail.com>
2025-11-08 13:23:52 +0900

    project started

---
+++ README.md
@@ -0,0 +1,56 @@
+## What is this
+This PlatformIO project combines the esp32-c3 super mini board and the inexpensive ST7789 display to show the current fine dust pollution situation (particulate matter) in Korea,
+using following public API: https://www.data.go.kr/data/15073861/openapi.do
+
+http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty
+
+## Screenshot
+![result](/screenshot/esp32-project.jpg)
+
+## Wiring for ESP32-C3 Super Mini
+ 
+| ST7789 |   ESP32-C3 Super Mini |
+|-|-|
+| GND    |    GND |
+| VCC    |    3V3 |
+| SCL    |    2   |
+| SDA    |    4   |
+| RES    |    0   |
+| DC     |    1   |
+| CS     |    5   |
+| BL     |    3V3 |
+
+
+## Display 
+[1.54" Color TFT Display Module 240x240 SPI Interface ST7789](https://ko.aliexpress.com/item/1005008625277253.html?spm=a2g0o.order_list.order_list_main.22.4abc140fw6y0zY&gatewayAdapt=glo2kor)
+
+## How to set up
+
+You need to apply for an API and receive an authentication key. And for the call to actually succeed, **you have to wait for a monthly server synchronization. It seems like it's usually reflected at the beginning of each month.** Yes, that's right. This is a really inconvenient process.
+
+Users must define **Wi-Fi connection information and authentication key values ​​for API** calls in a separate file. This header file is not included in source control.
+
+- First, **create the following file**:
+    ```cpp
+    include\private_defines.h
+    ```
+
+- Then, add the following to the file and change it to your own information
+    ```cpp
+    #ifndef PRIVATE_DEFINES_H 
+    #define PRIVATE_DEFINES_H
+
+    // XXX NOTE : Only 2.4 GHz wifi is supported !
+    const char* WIFI_SSID = "YOURS";
+    const char* WIFI_PASSWD = "YOURS";
+    const char* API_URL =
+        "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?returnType=json&numOfRows=1&pageNo=1&dataTerm=DAILY&ver=1.3&"
+        "stationName=YOURS&"
+        "serviceKey=YOURS";
+
+    // stationName => Example) "관악구" (URL must be encoded)
+    // eg) stationName=%EA%B4%80%EC%95%85%EA%B5%AC
+    // serviceKey => Authentication key received when requesting API
+
+    #endif 
+    ```
---
+++ src/main.cpp
@@ -0,0 +1,326 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>     
+#include <Adafruit_ST7789.h>  
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <Wire.h>
+
+#include "private_defines.h"
+
+void initBuf();
+void displayData();
+void connectWiFi();
+bool getDustData();
+String httpGETRequest();
+
+unsigned long lastTimeDust = 0;
+unsigned long lastTempHumTime = 0;
+unsigned long timerDelayDust = 120000;    // 2 min
+unsigned long timerDelayTempHum = 20000;  // 20 sec
+char oldMsrdtBuf[20];
+char msrdtBuf[20];
+char pm10Buf[20];
+char pm25Buf[20];
+char tempBuf[20];
+char humiBuf[20];
+int  pm10Val = 0;
+int  pm25Val = 0;
+#define TFT_WIDTH 240
+#define TFT_HEIGHT 240
+
+#define TFT_CS 5
+#define TFT_RST 0
+#define TFT_DC 1
+#define TFT_MOSI 4  // SDA 
+#define TFT_SCLK 2  // SCL
+
+// ST7789     ESP32-C3 Super Mini
+// GND        GND
+// VCC        3V3
+// SCL        2
+// SDA        4
+// RES        0
+// DC         1
+// CS         5
+// BL         3V3
+
+Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+// -----------------------------------------------------------------------------
+void setup(void) {
+    Serial.begin(9600);
+
+    memset(oldMsrdtBuf, 0x00, sizeof(oldMsrdtBuf));
+
+    display.init(TFT_WIDTH, TFT_HEIGHT);
+    display.fillScreen(ST77XX_BLACK);
+    display.setTextWrap(false);
+
+    display.setTextSize(3);
+    display.setRotation(2);
+    // landscape (wide) mode :
+    // https://learn.adafruit.com/adafruit-gfx-graphics-library/rotating-the-display
+    display.setCursor(0, 0);
+
+    initBuf();
+    connectWiFi();
+
+    if (getDustData()) {
+        displayData();
+    }
+}
+
+// -----------------------------------------------------------------------------
+void loop() {
+    bool isApiCallOk = false;
+    if ((millis() - lastTimeDust) > timerDelayDust) {
+        if (WiFi.status() == WL_CONNECTED) {
+            isApiCallOk = getDustData();
+        } else {
+            Serial.println("WiFi Disconnected");
+            connectWiFi();
+        }
+        lastTimeDust = millis();
+    }
+
+    if (isApiCallOk) {
+        displayData();
+    }
+
+    delay(5000);
+}
+
+// -----------------------------------------------------------------------------
+void initBuf() {
+    memset(msrdtBuf, 0x00, sizeof(msrdtBuf));
+    memset(pm10Buf, 0x00, sizeof(pm10Buf));
+    memset(pm25Buf, 0x00, sizeof(pm25Buf));
+}
+
+// -----------------------------------------------------------------------------
+void displayData() {
+    if (strncmp(oldMsrdtBuf, msrdtBuf, sizeof(msrdtBuf)) != 0) {
+        display.fillScreen(ST77XX_BLACK);
+        display.setCursor(0, 0);
+
+        display.setTextSize(2);
+        display.drawFastHLine(0, 0, display.width(), ST77XX_CYAN); //XXX
+        display.println("");
+
+        // date ---------------------------------------------------
+        display.setTextSize(3);
+        display.setTextColor(ST77XX_BLACK);
+        char imsiBuf[10];
+        memset(imsiBuf, 0x00, sizeof(imsiBuf));
+        strncpy(imsiBuf, msrdtBuf + 4, 2);
+        display.print("        ");
+        display.setTextColor(ST77XX_MAGENTA);
+        display.print(imsiBuf);
+        display.print("-");
+        memset(imsiBuf, 0x00, sizeof(imsiBuf));
+        strncpy(imsiBuf, msrdtBuf + 6, 2);
+        display.println(imsiBuf);
+
+        // minuite ---------------------------------------------------
+        display.setTextSize(5);
+        display.setTextColor(ST77XX_ORANGE);
+        memset(imsiBuf, 0x00, sizeof(imsiBuf));
+        strncpy(imsiBuf, msrdtBuf + 8, 2);
+        display.print(imsiBuf);
+        display.print(":");
+        memset(imsiBuf, 0x00, sizeof(imsiBuf));
+        strncpy(imsiBuf, msrdtBuf + 10, 2);
+        display.println(imsiBuf);
+
+        display.setTextSize(3);
+        display.drawFastHLine(0, 80, display.width(), ST77XX_CYAN); //XXX
+        display.println("");
+
+        // pm10 ------------------------------------------------------
+        // 좋음0~30
+        // 보통31~80
+        // 나쁨81~150
+        // 매우나쁨150 초과
+        display.fillRoundRect(1, 98, 100, 35, 5, display.color565(128, 128, 128));
+        display.setTextSize(3);
+        display.setTextColor(ST77XX_BLACK);
+        display.print(" PM10");
+        display.print("  ");
+        display.setTextSize(7);
+        if (pm10Val <= 30) {
+            display.setTextColor(ST77XX_BLUE);
+        } else if (pm10Val <= 80) {
+            display.setTextColor(display.color565(192, 192, 192)); //grey tone
+        } else if (pm10Val <= 150) {
+            display.setTextColor(ST77XX_ORANGE);
+        } else {
+            display.setTextColor(ST77XX_RED);
+        }
+        display.println(pm10Buf);
+
+        display.setTextSize(3);
+        display.drawFastHLine(0, 160, display.width(), ST77XX_CYAN);
+        display.println("");
+
+        // pm25 -------------------------------------------------------
+        // 초미세 등급지수범위 
+        //  0 좋음0~15
+        //  1 보통16~35
+        //  2 나쁨36~75
+        //  3 매우나쁨75 초과
+        display.fillRoundRect(1, 176, 100, 35, 5, display.color565(128, 128, 128));
+        display.setTextSize(3);
+        display.setTextColor(ST77XX_BLACK);
+        display.print(" PM25");
+        display.print("  ");
+        display.setTextSize(7);
+        if (pm25Val <= 15) {
+            display.setTextColor(ST77XX_BLUE);
+        } else if (pm25Val <= 35) {
+            display.setTextColor(display.color565(192, 192, 192));
+        } else if (pm25Val <= 75) {
+            display.setTextColor(ST77XX_ORANGE);
+        } else {
+            display.setTextColor(ST77XX_RED);
+        }
+        display.println(pm25Buf);
+
+        display.setTextSize(3);
+        display.drawFastHLine(0, 239, display.width(), ST77XX_CYAN); //XXX
+        display.println("");
+
+        snprintf(oldMsrdtBuf, sizeof(oldMsrdtBuf), "%s",
+            msrdtBuf);  // save old value
+    } else {
+        // display.fillScreen(ST77XX_BLACK);
+        // display.setCursor(0, 0);
+        // display.println("http error");
+    }
+}
+
+// -----------------------------------------------------------------------------
+void connectWiFi() {
+    // WiFi.setAutoReconnect(true);  //XXX test !!!
+    WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+
+    Serial.println("Connecting ");
+    display.println("Connecting ");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        display.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    display.fillScreen(ST77XX_BLACK);
+    display.setCursor(0, 0);
+    display.println("Connected");
+}
+
+// -----------------------------------------------------------------------------
+bool getDustData() {
+    String jsonData = httpGETRequest();
+    // Serial.println(jsonData);
+    if (strncmp("error", jsonData.c_str(), strlen("error")) == 0) {
+        Serial.println("http error !");
+        return false;
+    }
+    // Serial.println(jsonData);
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, jsonData);
+
+    if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+
+        display.fillScreen(ST77XX_BLACK);
+        display.setCursor(0, 0);
+        display.setTextSize(2);
+        display.print("Json failed:");
+        display.println(error.f_str());
+        return false;
+    }
+
+    //===========================================================
+    const char* rsltCode = doc["response"]["header"]["resultCode"];
+    const char* msrdt = doc["response"]["body"]["items"][0]["dataTime"]; // "2025-11-06 22:00"
+    int pm10 = doc["response"]["body"]["items"][0]["pm10Value"];
+    int pm25 = doc["response"]["body"]["items"][0]["pm25Value"];
+
+    initBuf();
+    snprintf(msrdtBuf, sizeof(msrdtBuf), "%s", msrdt);
+    for (int src = 0, dst = 0; src < sizeof(msrdtBuf); src++) {
+        if (msrdtBuf[src] != ':' && msrdtBuf[src] != '-' && msrdtBuf[src] != ' ') {
+            msrdtBuf[dst++] = msrdtBuf[src];
+        }
+    }
+    // Serial.println(msrdtBuf); // "202511062200"
+    pm10Val = pm10;
+    pm25Val = pm25;
+    snprintf(pm10Buf, sizeof(pm10Buf), "%d", pm10);
+    snprintf(pm25Buf, sizeof(pm25Buf), "%d", pm25);
+    // snprintf(pm25Buf, sizeof(pm25Buf), "%.1f", pm25);
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+String httpGETRequest() {
+    Serial.println("http request...");
+
+    for (int retryCnt = 0; retryCnt < 5; retryCnt++) {
+        WiFiClient client;
+        HTTPClient http;
+        http.setConnectTimeout(5000);
+        http.setTimeout(10000);  // milliseconds
+        http.begin(client, API_URL);
+        delay(500);
+
+        int httpResponseCode = http.GET();
+        String payload = "{}";
+
+        if (httpResponseCode > 0) {
+            if (httpResponseCode == HTTP_CODE_OK) {
+                Serial.print("HTTP Response code: ");
+                Serial.println(httpResponseCode);
+                payload = http.getString();
+                http.end();  // Free resources
+                return payload;
+            } else {
+                Serial.printf("-http GET failed, error: %s\n",
+                    http.errorToString(httpResponseCode).c_str());
+                Serial.println(http.errorToString(httpResponseCode).c_str());
+
+                display.fillScreen(ST77XX_BLACK);
+                display.setCursor(0, 0);
+                display.setTextSize(2);
+                display.println("HTTP GET failed:");
+                display.println(httpResponseCode);
+                display.println(http.errorToString(httpResponseCode).c_str());
+                http.end();  // Free resources
+            }
+        } else {
+            Serial.printf("=http GET failed, error: %s\n",
+                http.errorToString(httpResponseCode).c_str());
+            Serial.println(http.errorToString(httpResponseCode).c_str());
+
+            display.fillScreen(ST77XX_BLACK);
+            display.setCursor(0, 0);
+            display.setTextSize(2);
+            display.println("HTTP GET failed:");
+            display.println(httpResponseCode);
+            display.println(http.errorToString(httpResponseCode).c_str());
+            http.end();  // Free resources
+        }
+        delay(1000);
+        Serial.println("Retrying...");
+        display.println("HTTP GET retrying...");
+        continue; //retry
+    } //for 
+    memset(oldMsrdtBuf, 0x00, sizeof(oldMsrdtBuf));
+    return "error";
+}
